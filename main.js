d030280512069ebf702817154addfa614c6b12b0
RafaGarbin <107377830+RafaGarbin@users.noreply.github.com>
2024-05-05 23:06:02 -0300

    ok

--- main.js
+++ main.js
@@ -1,7 +1,7 @@
 
 
 function verificarResposta(resposta) {
-    if (resposta === 3) {
+    if (resposta === 2) {
         document.querySelector ('#som-certo').play(  );
     } else {
         document.getElementById('resultado').innerText = 'Você errou!';
