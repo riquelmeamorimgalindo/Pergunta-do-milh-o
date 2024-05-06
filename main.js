b2c7b666a4bda6d5b0765312aed55d3ec0cee436
RafaGarbin <107377830+RafaGarbin@users.noreply.github.com>
2024-05-05 23:18:08 -0300

    dd

--- main.js
+++ main.js
@@ -1,11 +1,11 @@
 function verificarResposta(resposta) {
     if (resposta === 3) {
         document.getElementById('resultado').innerText = 'Acertou!';
-        var audio = new Audio('audio-certo');
+        var audio = new Audio('silvio-santos-certa-resposta.mp3');
         audio.play();
     } else {
         document.getElementById('resultado').innerText = 'Você errou!';
-        var audio = new Audio('audio-errado');
+        var audio = new Audio('silvio-santos-que-pena-voce-errou.mp3');
         audio.play();
     }
 }
